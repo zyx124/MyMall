f8e582dcc62a6ddd5715bb82027431594ad033e7
zyx124 <zhaoyuxin124@gmail.com>
2020-10-14 23:08:10 -0400

    divide the modules and set the root module with pom file.

---
+++ warehouse/src/test/java/com/zyx/mall/warehouse/WarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyx.mall.warehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
