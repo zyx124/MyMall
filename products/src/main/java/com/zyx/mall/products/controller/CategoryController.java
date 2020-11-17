07a1bf95656774107fbf87ff7cc72f206e3a735d
zyx <zhaoyuxin124@gmail.com>
2020-11-16 22:18:05 -0500

    add categorization to the products based on class

--- products/src/main/java/com/zyx/mall/products/controller/CategoryController.java
+++ products/src/main/java/com/zyx/mall/products/controller/CategoryController.java
@@ -1,6 +1,7 @@
 package com.zyx.mall.products.controller;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,14 +32,12 @@ public class CategoryController {
     private CategoryService categoryService;
 
     /**
-     * 列表
+     * find all the categories and sub-categories
      */
-    @RequestMapping("/list")
-    // @RequiresPermissions("products:category:list")
-    public R list(@RequestParam Map<String, Object> params){
-        PageUtils page = categoryService.queryPage(params);
-
-        return R.ok().put("page", page);
+    @RequestMapping("/list/tree")
+    public R list(){
+        List<CategoryEntity> entities = categoryService.listWithTree();
+        return R.ok().put("data", entities);
     }
 
 
--- products/src/main/java/com/zyx/mall/products/service/impl/CategoryServiceImpl.java
+++ products/src/main/java/com/zyx/mall/products/service/impl/CategoryServiceImpl.java
@@ -1,7 +1,12 @@
 package com.zyx.mall.products.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -16,6 +21,9 @@ import com.zyx.mall.products.service.CategoryService;
 @Service("categoryService")
 public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity> implements CategoryService {
 
+//    @Autowired
+//    CategoryDao categoryDao;
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
         IPage<CategoryEntity> page = this.page(
@@ -24,6 +32,35 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         );
 
         return new PageUtils(page);
+    }
+
+    @Override
+    public List<CategoryEntity> listWithTree() {
+        // get all the categories
+        List<CategoryEntity> entities = baseMapper.selectList(null);
+
+        // construct a tree
+        List<CategoryEntity> level1Menu = entities.stream().filter(categoryEntity -> categoryEntity.getParentCid() == 0).map((menu)->{
+            menu.setChildren(getChildren(menu, entities));
+            return menu;
+        }).sorted((menu1, menu2)->{
+            return (menu1.getSort() == null ? 0 : menu1.getSort() )- (menu2.getSort() == null ? 0: menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return level1Menu;
+    }
+
+    private List<CategoryEntity> getChildren(CategoryEntity root, List<CategoryEntity> all) {
+        List<CategoryEntity> children = all.stream().filter(categoryEntity -> {
+            return categoryEntity.getParentCid() == root.getCatId();
+        }).map(categoryEntity -> {
+            categoryEntity.setChildren(getChildren(categoryEntity, all));
+            return categoryEntity;
+        }).sorted((menu1, menu2) -> {
+            return (menu1.getSort() == null ? 0 : menu1.getSort() )- (menu2.getSort() == null ? 0: menu2.getSort());
+        }).collect(Collectors.toList());
+
+        return children;
     }
 
 }
