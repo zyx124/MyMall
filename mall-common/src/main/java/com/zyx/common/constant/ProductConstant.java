42c9c5a16dd90de99f12f5c14a918ffb08a17b3c
zyx <zhaoyuxin124@gmail.com>
2021-01-25 20:11:32 -0500

    add constant for products

--- mall-common/src/main/java/com/zyx/common/constant/ProductConstant.java
+++ mall-common/src/main/java/com/zyx/common/constant/ProductConstant.java
@@ -1,4 +1,28 @@
 package com.zyx.common.constant;
 
 public class ProductConstant {
+
+    public enum AttrEnum {
+        ATTR_TYPE_BASE(1, "base attribute"), ATTR_TYPE_SALE(0, "attribute for sale");
+
+        private int code;
+        private String msg;
+
+        AttrEnum(int code, String msg) {
+            this.code = code;
+            this.msg = msg;
+        }
+
+        public int getCode() {
+            return code;
+        }
+
+        public String getMsg() {
+            return msg;
+        }
+
+
+    }
+
+
 }
