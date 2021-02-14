145f90544c38a484b9cc946251f670b397aad336
zyx <zhaoyuxin124@gmail.com>
2021-02-13 20:41:47 -0500

    correct data types for ids and decimals

--- products/src/main/java/com/zyx/mall/products/vo/Bounds.java
+++ products/src/main/java/com/zyx/mall/products/vo/Bounds.java
@@ -3,28 +3,20 @@
   */
 package com.zyx.mall.products.vo;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
 /**
  * Auto-generated: 2021-02-12 21:33:37
  *
  * @author bejson.com (i@bejson.com)
  * @website http://www.bejson.com/java2pojo/
  */
+@Data
 public class Bounds {
 
-    private int buyBounds;
-    private int growBounds;
-    public void setBuyBounds(int buyBounds) {
-         this.buyBounds = buyBounds;
-     }
-     public int getBuyBounds() {
-         return buyBounds;
-     }
-
-    public void setGrowBounds(int growBounds) {
-         this.growBounds = growBounds;
-     }
-     public int getGrowBounds() {
-         return growBounds;
-     }
+    private BigDecimal buyBounds;
+    private BigDecimal growBounds;
 
 }
