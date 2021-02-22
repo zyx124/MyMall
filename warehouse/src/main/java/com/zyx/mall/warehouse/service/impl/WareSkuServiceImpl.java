626e26df623e1099091536dd6452401e7f6bc2c5
zyx <zhaoyuxin124@gmail.com>
2021-02-21 22:24:22 -0500

    finish search function in warehouse maintainance

--- warehouse/src/main/java/com/zyx/mall/warehouse/service/impl/WareSkuServiceImpl.java
+++ warehouse/src/main/java/com/zyx/mall/warehouse/service/impl/WareSkuServiceImpl.java
@@ -1,5 +1,7 @@
 package com.zyx.mall.warehouse.service.impl;
 
+import com.zyx.mall.warehouse.entity.WareInfoEntity;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import java.util.Map;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -16,11 +18,25 @@ import com.zyx.mall.warehouse.service.WareSkuService;
 @Service("wareSkuService")
 public class WareSkuServiceImpl extends ServiceImpl<WareSkuDao, WareSkuEntity> implements WareSkuService {
 
+
     @Override
     public PageUtils queryPage(Map<String, Object> params) {
+
+        QueryWrapper<WareSkuEntity> wrapper = new QueryWrapper<>();
+        String skuId = (String) params.get("skuId");
+
+        if (!StringUtils.isEmpty(skuId)) {
+            wrapper.eq("sku_id", skuId);
+        }
+
+        String wareId = (String) params.get("wareId");
+        if (!StringUtils.isEmpty(wareId)) {
+            wrapper.eq("ware_id", wareId);
+        }
+
         IPage<WareSkuEntity> page = this.page(
                 new Query<WareSkuEntity>().getPage(params),
-                new QueryWrapper<WareSkuEntity>()
+                wrapper
         );
 
         return new PageUtils(page);
