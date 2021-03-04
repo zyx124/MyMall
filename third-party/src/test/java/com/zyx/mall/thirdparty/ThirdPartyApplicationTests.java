030fb96b6fd5e9f8be1865407fff4ae3590d77ab
zyx <zhaoyuxin124@gmail.com>
2021-03-03 21:17:08 -0500

    remove the leak of secret keys

--- third-party/src/test/java/com/zyx/mall/thirdparty/ThirdPartyApplicationTests.java
+++ third-party/src/test/java/com/zyx/mall/thirdparty/ThirdPartyApplicationTests.java
@@ -22,8 +22,7 @@ class ProductsApplicationTests {
     public void upload() throws FileNotFoundException {
 //		String endpoint = "http://oss-cn-beijing.aliyuncs.com";
 //// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
-//		String accessKeyId = "LTAI4G7Xysb2DQDzJunL3jQj";
-//		String accessKeySecret = "odimylDuJWMiokJlJCSVlCY0Lni2GE";
+
 //
 //// 创建OSSClient实例。
 //		OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
