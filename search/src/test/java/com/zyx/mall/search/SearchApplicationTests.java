48b04785729be2c7f1cb2941cba2223ffd24453b
zyx <zhaoyuxin124@gmail.com>
2021-03-09 22:47:36 -0500

    add search to nacos and test it. Ccorrect the version in pom file.

--- search/src/test/java/com/zyx/mall/search/SearchApplicationTests.java
+++ search/src/test/java/com/zyx/mall/search/SearchApplicationTests.java
@@ -1,13 +1,24 @@
 package com.zyx.mall.search;
 
+import org.elasticsearch.client.RestHighLevelClient;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class SearchApplicationTests {
 
+    @Qualifier("esRestClient")
+    @Autowired
+    private RestHighLevelClient client;
+
     @Test
     void contextLoads() {
+        System.out.println(client);
     }
 
 }
