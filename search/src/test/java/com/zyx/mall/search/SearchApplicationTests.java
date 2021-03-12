1bb012b6d6ef1c16bdf688973a04016b717b0fc2
zyx <zhaoyuxin124@gmail.com>
2021-03-11 22:04:18 -0500

    add test for search with aggrations success

--- search/src/test/java/com/zyx/mall/search/SearchApplicationTests.java
+++ search/src/test/java/com/zyx/mall/search/SearchApplicationTests.java
@@ -6,9 +6,19 @@ import lombok.Data;
 import net.minidev.json.JSONArray;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.ElasticsearchClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentType;
+import org.elasticsearch.index.query.QueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.aggregations.AggregationBuilders;
+import org.elasticsearch.search.aggregations.bucket.terms.TermsAggregationBuilder;
+import org.elasticsearch.search.aggregations.metrics.AvgAggregationBuilder;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,6 +26,7 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.naming.directory.SearchResult;
 import java.io.IOException;
 
 
@@ -30,6 +41,42 @@ class SearchApplicationTests {
     @Test
     void contextLoads() {
         System.out.println(client);
+    }
+
+    @Test
+    void searchData() throws IOException {
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices("bank");
+        SearchSourceBuilder builder = new SearchSourceBuilder();
+        // 1. build the search conditions
+        builder.query(QueryBuilders.matchQuery("address", "mill"));
+
+        // 2. Aggregate with age
+        TermsAggregationBuilder ageAgg = AggregationBuilders.terms("ageAgg").field("age").size(10);
+        builder.aggregation(ageAgg);
+
+        // 3. calculate avg salary
+        AvgAggregationBuilder balanceAvg = AggregationBuilders.avg("balanceAvg").field("balance");
+        builder.aggregation(balanceAvg);
+
+        System.out.println("search conditions: " + builder.toString());
+
+        searchRequest.source(builder);
+
+        // execute
+        SearchResponse searchResponse = client.search(searchRequest, ElasticSearchConfig.COMMON_OPTIONS);
+
+        // analyse the result
+        System.out.println(searchResponse.toString());
+        SearchHits hits = searchResponse.getHits();
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit: searchHits) {
+            String sourceAsString = hit.getSourceAsString();
+            System.out.println(sourceAsString);
+
+        }
+
+
     }
 
     /**
