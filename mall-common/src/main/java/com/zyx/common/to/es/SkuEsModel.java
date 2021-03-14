66e47775f681694cbb5bd806011b9e5ce972f47f
zyx <zhaoyuxin124@gmail.com>
2021-03-13 22:52:43 -0500

    add the sku object for elasticsearch

--- mall-common/src/main/java/com/zyx/common/to/es/SkuEsModel.java
+++ mall-common/src/main/java/com/zyx/common/to/es/SkuEsModel.java
@@ -1,4 +1,45 @@
 package com.zyx.common.to.es;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Data
 public class SkuEsModel {
+    private Long skuId;
+
+    private Long spuId;
+
+    private String skuTitle;
+
+    private BigDecimal skuPrice;
+
+    private String skuImg;
+
+    private Long saleCount;
+
+    private Boolean hasStock;
+
+    private Long hotScore;
+
+    private Long brandId;
+
+    private Long catalogId;
+
+    private String brandName;
+
+    private String brandImg;
+
+    private String catalogName;
+
+    private List<Object> attrs;
+
+    @Data
+    public static class Attrs {
+
+        private Long attrId;
+        private String attrName;
+        private String attrValue;
+    }
 }
