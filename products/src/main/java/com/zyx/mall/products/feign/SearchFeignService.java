0bde5f5d528d4f961b311ec86f0f6ab73026b5d4
zyx <zhaoyuxin124@gmail.com>
2021-03-20 22:45:40 -0400

    add feign service for search and warehouse info retrieve

--- products/src/main/java/com/zyx/mall/products/feign/SearchFeignService.java
+++ products/src/main/java/com/zyx/mall/products/feign/SearchFeignService.java
@@ -1,7 +1,16 @@
 package com.zyx.mall.products.feign;
 
+import com.zyx.common.to.es.SkuEsModel;
+import com.zyx.common.utils.R;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient
-public interface SearchFeighService {
+import java.util.List;
+
+@FeignClient("search")
+public interface SearchFeignService {
+
+    @PostMapping("/search/save/product")
+    R productStatusUp(@RequestBody List<SkuEsModel> skuEsModels);
 }
--- products/src/main/java/com/zyx/mall/products/feign/WareFeignService.java
+++ products/src/main/java/com/zyx/mall/products/feign/WareFeignService.java
@@ -1,4 +1,16 @@
 package com.zyx.mall.products.feign;
 
+import com.zyx.common.to.SkuHasStockVO;
+import com.zyx.common.utils.R;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
+@FeignClient("warehouse")
 public interface WareFeignService {
+
+    @PostMapping("/warehouse/waresku/hasstock")
+    R<List<SkuHasStockVO>> getSkuHasStock(@RequestBody List<Long> skuIds);
 }
