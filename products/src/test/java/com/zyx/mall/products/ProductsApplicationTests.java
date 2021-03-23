ded77fee91f10f30ae1c74ac59c50f13277c30e7
zyx <zhaoyuxin124@gmail.com>
2021-03-22 23:19:23 -0400

    remove access keys

--- products/src/test/java/com/zyx/mall/products/ProductsApplicationTests.java
+++ products/src/test/java/com/zyx/mall/products/ProductsApplicationTests.java
@@ -43,8 +43,7 @@ class ProductsApplicationTests {
 
 	@Test
 	public void upload() throws FileNotFoundException {
-//		String endpoint = "http://oss-cn-beijing.aliyuncs.com";
-//// 云账号AccessKey有所有API访问权限，建议遵循阿里云安全最佳实践，创建并使用RAM子账号进行API访问或日常运维，请登录 https://ram.console.aliyun.com 创建。
+
 
 //
 //// 创建OSSClient实例。
